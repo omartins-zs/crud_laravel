1dc451d2c79417cb949aaac0fa766a9ce6b73d2b
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-12 12:41:26 -0300

    Add Bootstrap ao Template Blade do CRUD

--- laravel/resources/views/templates/template.blade.php
+++ laravel/resources/views/templates/template.blade.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crud com Laravel</title>
+
+    {{-- Bootstrap --}}
+    <link rel="stylesheet" href="{{url('assets/bootstrap/css/bootstrap.min.css')}}">
+
 </head>
 
 <body>
