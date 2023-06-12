c5ec56d8cd59acabcb7647a1bc123e1ddfd574a2
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-12 12:31:52 -0300

    Criando Rota Index, Igual o Tutorial

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Mostra o controller e o Metodo que vem depois do @
+// Metodo do Video
+Route::get('/', 'BookController@index');
