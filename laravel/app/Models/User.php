6d60731735ada93037865f395ff25bfc84af3a50
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-14 11:10:23 -0300

    Add Relacionamento do Book e User

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function relBooks()
+    {
+        return $this->hasMany('App\Models\ModelBook', 'id_user');
+    }
 }
