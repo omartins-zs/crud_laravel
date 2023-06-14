6cc0e5bee47ffac040711a54c4f10e098c99fcb9
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-14 14:58:57 -0300

    Add Validações de Required nos campos do Form Book

--- laravel/resources/views/create.blade.php
+++ laravel/resources/views/create.blade.php
@@ -7,15 +7,15 @@
     <div class="col-8 m-auto">
         <form name="formCad" id="formCad" method="post" action="{{ url('books') }}">
             @csrf
-            <input class="form-control" type="text" name="title" id="title" placeholder="Título:"><br>
-            <select class="form-control" name="id_user" id="id_user">
+            <input class="form-control" type="text" name="title" id="title" placeholder="Título:" required><br>
+            <select class="form-control" name="id_user" id="id_user" required>
                 <option value="">Autor</option>
                 @foreach ($users as $user)
                     <option value="{{ $user->id }}">{{ $user->name }}</option>
                 @endforeach
             </select><br>
-            <input class="form-control" type="text" name="pages" id="pages" placeholder="Páginas:"><br>
-            <input class="form-control" type="text" name="price" id="price" placeholder="Preço:"><br>
+            <input class="form-control" type="text" name="pages" id="pages" required placeholder="Páginas:"><br>
+            <input class="form-control" type="text" name="price" id="price" required placeholder="Preço:"><br>
             <input class="btn btn-primary" type="submit" value="Cadastrar">
         </form>
     </div>
