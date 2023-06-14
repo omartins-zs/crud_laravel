4f8bfb498bd0909fd1431712ff55717e2a50bf16
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-14 14:54:42 -0300

    Add Permissao de Create Fillable de Book do Larave

--- laravel/app/Models/ModelBook.php
+++ laravel/app/Models/ModelBook.php
@@ -9,6 +9,8 @@ class ModelBook extends Model
 {
     use HasFactory;
     protected $table = 'book';
+    protected $fillable = ['title', 'id_user', 'pages', 'price'];
+
 
     public function relUsers()
     {
