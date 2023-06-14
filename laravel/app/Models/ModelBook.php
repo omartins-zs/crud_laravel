adbbdba682c434bc077905e9c24068e24f92a6f5
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-14 12:35:51 -0300

    Ajustando Model Book

--- laravel/app/Models/ModelBook.php
+++ laravel/app/Models/ModelBook.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\Models;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -10,9 +10,8 @@ class ModelBook extends Model
     use HasFactory;
     protected $table = 'book';
 
-
     public function relUsers()
     {
-        return $this->hasOne('App\User', 'id', 'id_user');
+        return $this->hasOne('App\Models\User', 'id', 'id_user');
     }
 }
