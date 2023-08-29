be420b8516762c5a117fef3ec2921adb2edb1619
Gabriel Martins <gabrielmatheus209@gmail.com>
2023-08-29 00:19:22 -0300

    Add + Livros no BookSeeder

--- laravel/database/seeders/BookSeeder.php
+++ laravel/database/seeders/BookSeeder.php
@@ -39,5 +39,29 @@ class BookSeeder extends Seeder
             'price' => '99.99',
             'id_user' => '1',
         ]);
+        $book->create([
+            'title' => 'As aventuras de PI',
+            'pages' => '126',
+            'price' => '7.99',
+            'id_user' => '1',
+        ]);
+        $book->create([
+            'title' => 'Pais das Maravilhas',
+            'pages' => '54',
+            'price' => '20.99',
+            'id_user' => '2',
+        ]);
+        $book->create([
+            'title' => 'Velozes e Furiosos',
+            'pages' => '20',
+            'price' => '100.99',
+            'id_user' => '3',
+        ]);
+        $book->create([
+            'title' => 'Harry Potter - Camara Secreta',
+            'pages' => '189',
+            'price' => '67.99',
+            'id_user' => '3',
+        ]);
     }
 }
